977e8852a136ed10eb0c671221cc280654d2ff36
Pierre Segalen <pierre.segalen@gmail.com>
2021-01-11 13:51:54 +0100

    Course 2 - Added a 10 minutes persisted cache on Lists data

--- course1/todo/src/data/ListsContext.js
+++ course1/todo/src/data/ListsContext.js
@@ -1,6 +1,9 @@
 import { createContext, useState } from "react";
 import apiHelper from "../apiHelper";
 
+const LISTS_STORAGE_KEY = "TODO_LISTS";
+const LISTS_LAST_FETCH_DATE_KEY = "LISTS_LAST_FETCH_DATE_KEY";
+
 export const ListsContext = createContext({
   lists: [],
   setLists: () => {},
@@ -8,19 +11,40 @@ export const ListsContext = createContext({
 });
 
 export const ListsProvider = ({ children }) => {
-  const [lists, setLists] = useState([]);
+  const storageData = localStorage.getItem(LISTS_STORAGE_KEY);
+  const initialState = storageData ? JSON.parse(storageData) : [];
+  const [lists, setLists] = useState(initialState);
+  const lastFetchDate = localStorage.getItem(
+    LISTS_LAST_FETCH_DATE_KEY
+  );
+
+  const finalSetLists = (newLists) => {
+    localStorage.setItem(LISTS_STORAGE_KEY, JSON.stringify(newLists));
+    setLists(newLists);
+  };
+  const tenMinutes = 10 * 60 * 1000;
 
   const getLists = () => {
-    if (lists.length === 0) {
+    if (
+      lists.length === 0 ||
+      !lastFetchDate ||
+      lastFetchDate < new Date().getTime() - tenMinutes
+    ) {
       // Get the lists on the API
       apiHelper.getLists().then((apiLists) => {
-        setLists(apiLists);
+        finalSetLists(apiLists);
+        localStorage.setItem(
+          LISTS_LAST_FETCH_DATE_KEY,
+          new Date().getTime()
+        );
       });
     }
   };
 
   return (
-    <ListsContext.Provider value={{ lists, setLists, getLists }}>
+    <ListsContext.Provider
+      value={{ lists, setLists: finalSetLists, getLists }}
+    >
       {children}
     </ListsContext.Provider>
   );
